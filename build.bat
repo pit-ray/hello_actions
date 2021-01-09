55093c6dcc03d5380692f9186e9af0dbf65f4c02
pit-ray <pit-ray@outlook.com>
2021-01-10 03:09:30 +0900

    refacted

--- build.bat
+++ build.bat
@@ -1,8 +1,5 @@
-@if not exist debug (
-    @mkdir debug
-)
+@if not exist debug (@mkdir debug)
 cd debug
-
 cmake -DCMAKE_BUILD_TYPE=Debug -G "MinGW Makefiles" ..
 cmake --build . --config Debug
 cd ..
